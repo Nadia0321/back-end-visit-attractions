114ddde220edb50fd2321204813e17149bbdc82f
Nadia Mirzai <nadia.m.mirzai@gmail.com>
2023-07-28 12:08:03 -0400

    add serilizers

--- visit_attractions/serializers.py
+++ visit_attractions/serializers.py
@@ -3,8 +3,22 @@ from .places import Place
 from .attractions import Attraction
 from .comments import Comment
 
+class PlaceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Place
+        fields = ['id', 'name', 'gmail', 'password']
 
 class PlaceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Place
         fields = ['id', 'name']
+
+class AttractionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attraction
+        fields = ['id', 'name', 'likes', 'dislike', 'description', 'favorite', 'place_id','user_id']
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'description', 'attraction_id','user_id']
--- visit_attractions/user.py
+++ visit_attractions/user.py
@@ -4,4 +4,5 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=100)
     gmail = models.CharField(max_length=1000)
+    password = models.CharField(max_length=1000, default="")
     
